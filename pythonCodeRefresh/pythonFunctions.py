2534b9752b9bd6b7ddc07bb72e0a4e29c907f346
MBriese <mindi.briese@gmail.com>
2024-03-16 17:07:04 -0700

    format fixes

--- pythonCodeRefresh/pythonFunctions.py
+++ pythonCodeRefresh/pythonFunctions.py
@@ -10,12 +10,14 @@ def user_age():
 
 
 def add(x, y):
-    additionresult: object = x + y
-    print(f"this is the result {additionresult}")
+    return x + y
 
 
-def addThem(*args):
-    return sum(args)
+def addThem(dict):
+    sums = 0
+    for arg in dict.values():
+        sums = sums + arg
+    return sums
 
 
 def greetingName(name):
@@ -40,6 +42,14 @@ def addTwo(x, y):
     return x + y
 
 
+def addList(*lst):
+    total = 0
+    for num in range(0, len(lst)):
+        total = total + lst[num]
+    print(f"sum of elements in list is {total}")
+    return total
+
+
 def divide_result(dividend, divisor):
     if divisor != 0:
         result = int(dividend / divisor)
@@ -55,6 +65,16 @@ def multiplyMultipleArgs(*args):
     return total
 
 
+def apply(*args, operator):
+    print(f"Operator is {operator} and *args list {args}")
+    if operator == "*":
+        return multiplyMultipleArgs(*args)
+    elif operator == "+":
+        return sum(args)
+    else:
+        return "No valid operators provided to apply()."
+
+
 hello()
 user_age()
 addTwo(1, 1)
@@ -66,7 +86,7 @@ divide(9, 3)
 divide(3, 0)
 
 add(5, 8)
-sumfromAddEmUp = addThem(5, 8)
+sumfromAddEmUp = addTwo(5, 8)
 print(f"the sum from add them up is {sumfromAddEmUp}")
 print("using returnAddEnds result below")
 print(addTwo(5, 8))
@@ -79,24 +99,16 @@ print(f"multiply variable number of arguments sent as params (1 ,3, 5) to multip
 
 # destructuring lists into functions
 numsList = [3, 5]
-print(f"destructure list into an add function {numsList} is {addThem(*numsList)}")
+sumOfList = addList(*numsList)
+print(f"destructure list into an add function {numsList} is {sumOfList}")
 
 # destructure dictionary into function using **
 numsDictionary = {"a": 15, "b": 25}
 
 print(f"using a dictionary {numsDictionary} for params in sum function) ")
 
-
-def apply(*args, operator):
-    print(f"Operator is {operator} and *args list {args}")
-    if operator == "*":
-        return multiplyMultipleArgs(*args)
-    elif operator == "+":
-        return addThem(*args)
-    else:
-        return "No valid operators provided to apply()."
-
-
-anotherDictionary = {"a": 15, "b": 25, "c": 1}
-
-print(f"add numbers in dictionary is {apply(*anotherDictionary, operator="+")}")
+# problems with dictionary - try later anotherDictionary = {"a": 15, "b": 25, "c": 1}
+anotherList = [15, 25, 1]
+# print("Sum: ", apply(*anotherDictionary, operator="+"))
+print("Sum: ", apply(15, 25, 1, operator = "+") )
+print("Multiply: ", apply(15, 25, 1, operator = "*"))
